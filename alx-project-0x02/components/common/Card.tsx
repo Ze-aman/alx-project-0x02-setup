d138be1d6027dac5dd3ae2cc99657050f2137b17
Ze-aman <aman09dida@gmail.com>
2025-11-23 17:20:39 +0300

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,6 +1,6 @@
 // components/common/Card.tsx
 import React from 'react';
-import { CardProps } from '../../interfaces'; // Import the interface
+import { type CardProps } from '../../interfaces'; // <-- UPDATED to use 'import type'
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
